d2f19bfcab57d8dd3bfabc30325f016f7a76f311
francis-tang <13006882716@163.com>
2019-08-18 22:14:48 +0800

    log ouput

--- http/main.go
+++ http/main.go
@@ -10,11 +10,20 @@ import (
 	"os/signal"
 	"syscall"
 	"fmt"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 )
 
 func main(){
 	nsa:=api.NewNameServiceApi()
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stdout))
+	}
+	level.Info(logger).Log("starting","on")
 
+
+	appLogger := log.With(logger,"app")
 	var g run.Group
 	{
 		server:=http.Server{
@@ -23,6 +32,7 @@ func main(){
 		}
 
 		g.Add(func() error {
+			level.Info(appLogger).Log("server","127.0.0.1:25000")
 			return server.ListenAndServe()
 		},func (error){
 			ctx,cancel := context.WithTimeout(context.Background(),time.Second)
